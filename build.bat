9236d81dba042c6a2351b6d97d020e479ff28fc0
donomii <donomii@gmail.com>
2021-06-20 10:47:28 +0200

    Start the rename

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-del *.exe
-del *.exe~
-go build ..\
+del build\*.exe
+del build\*.exe~
+go build -a -o build/ .
 "c:\Program Files (x86)\Inno Script Studio\ISStudio.exe" -compile installer.iss
--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-vort-winfs.exe z:\ http://127.0.0.1:80/
+build\vort-winfs.exe z:\ http://127.0.0.1:80/
